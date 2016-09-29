2624ac2b0091c77df1d595d8a1e0e1eea572c959
damasterham <cmeisterham@gmail.com>
2016-09-29 11:10:32 +0200

    Removec IMovable since its method isValidMove is implemented as an abstract method in Piece

--- src/Chess/IMovable.java
+++
@@ -1,9 +0,0 @@
-package Chess;//
-
-//Created by DaMasterHam on 20-09-2016.
-//
-public interface IMovable
-{
-//    public boolean isValidMove(Point to);
-    public boolean isValidMove(Point to);
-}
