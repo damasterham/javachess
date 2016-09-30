edfb4bb5f7646bd4b9ad6a3a80afca7857cb04a9
damasterham <cmeisterham@gmail.com>
2016-09-30 13:03:47 +0200

    Added Move.java, which represents a from Position and a to Position To be used in player as a move

---
+++ src/Chess/Move.java
@@ -0,0 +1,30 @@
+package Chess;//
+
+//Created by DaMasterHam on 28-09-2016.
+//
+public class Move
+{
+    private Point from;
+    private Point to;
+
+    public Point getFrom()
+    {
+        return from;
+    }
+
+    public Point getTo()
+    {
+        return to;
+    }
+
+
+    public void setFrom(Point from)
+    {
+        this.from = from;
+    }
+
+    public void setTo(Point to)
+    {
+        this.to = to;
+    }
+}
