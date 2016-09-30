bc0e1057c191719880032cad2604c20dab08e8ba
damasterham <cmeisterham@gmail.com>
2016-09-30 16:53:52 +0200

    Reformatted code for visiblity, added moves and moveslist for the users current and past moves. The turn field is now changed via startTurn endTurn methods instead of seetter

--- src/Chess/Player.java
+++ src/Chess/Player.java
@@ -2,6 +2,9 @@ package Chess;//
 
 import Chess.Pieces.Piece;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Created by DaMasterHam on 20-09-2016.
 //
 public class Player
@@ -9,42 +12,105 @@ public class Player
     private String name;
     private String color;
     private boolean turn;
+    private Move move;
+    private List<Move> pastMoves;
 
-    public void setColor(String color)
+    public Player()
     {
-        this.color = color;
+        pastMoves = new ArrayList<Move>();
     }
 
-    public String getColor()
+
+    // Accessors
+    public String       getName()
+    {
+        return name;
+    }
+
+    public String       getColor()
     {
         return color;
     }
 
-    public String getName()
-    {
-
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public boolean isTurn()
+    public boolean      isTurn()
     {
         return turn;
     }
 
-    public void setTurn(boolean turn)
+    public Move         getMove()
     {
-        this.turn = turn;
+        return move;
     }
 
-    public  boolean owns(Piece piece)
+    public Point        moveFrom()
+    {
+        return move.getFrom();
+    }
+
+    public Point        moveTo()
+    {
+        return move.getTo();
+    }
+
+    public boolean      owns(Piece piece)
     {
         return (color.toLowerCase().equals(piece.getColor().toLowerCase()));
     }
+
+    public List<Move>   getPastMoves()
+    {
+        return pastMoves;
+    }
+
+
+    // Mutators
+    public void         setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void         setColor(String color)
+    {
+        this.color = color;
+    }
+
+    public void         startTurn()
+    {
+        turn = true;
+    }
+
+    public void         endTurn()
+    {
+        pastMoves.add(move);
+        move = null;
+        turn = false;
+    }
+
+    public void         setMove(Move move)
+    {
+        this.move = move;
+    }
+
+    public void         newMove()
+    {
+        move = new Move();
+    }
+
+    public void         clearMove()
+    {
+        move = null;
+    }
+
+    public void         moveFrom(Point from)
+    {
+        move.setFrom(from);
+    }
+
+    public void         moveTo(Point to)
+    {
+        move.setTo(to);
+    }
+
 
 
 
