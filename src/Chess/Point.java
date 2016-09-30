ee38b599ce27f30a374773bccd839541c91bc103
damasterham <cmeisterham@gmail.com>
2016-09-30 16:57:19 +0200

    Reformatted code for visibility

--- src/Chess/Point.java
+++ src/Chess/Point.java
@@ -7,10 +7,18 @@ import java.util.*;
 //
 public class Point
 {
+    private static final int LIMIT = 8;
+
     private int x;
     private int y;
 
     public Point(){}
+
+    public Point(int x, int y)
+    {
+        this.y = y;
+        this.x = x;
+    }
 
     public Point(String input)
     {
@@ -31,19 +39,29 @@ public class Point
         }
     }
 
-    public Point(int x, int y)
+
+    // Accessors
+    public int          getX()
     {
-        this.y = y;
-        this.x = x;
+        return x;
     }
 
+    public int          getY()
+    {
+        return y;
+    }
 
-    private Point getDiff(Point other)
+    private Point       getDiff(Point other)
     {
         return new Point(other.getX() - x, other.getY() - y);
     }
 
-    private int getSteps(Point other)
+    private float       getIncrement(int value, int steps)
+    {
+        return value / (float) steps;
+    }
+
+    private int         getSteps(Point other)
     {
         if (other.getX() > other.getY())
             return other.getX();
@@ -51,43 +69,7 @@ public class Point
             return other.getY();
     }
 
-    private float getIncrement(int value, int steps)
-    {
-        return value / (float) steps;
-    }
-
-    public int getX()
-    {
-        return x;
-    }
-
-    public void setX(int x)
-    {
-        this.x = x;
-    }
-
-    public void setX(char letter)
-    {
-        this.x = CharIntMap.getIndex(letter);
-    }
-
-    public int getY()
-    {
-        return y;
-    }
-
-    public void setY(int y)
-    {
-        this.y = y;
-    }
-
-    public void setY(char letter)
-    {
-        this.y = CharIntMap.getIndex(letter);
-    }
-
-
-    public List<Point> getPointsBetween(Point to)
+    public List<Point>  getPointsBetween(Point to)
     {
         List<Point> list = new ArrayList<Point>();
         int steps;
@@ -113,15 +95,50 @@ public class Point
         return list;
     }
 
-    public String toChess()
+
+    // Mutators
+    public void         setX(int x)
+    {
+        if (x >= 0 && x < LIMIT)
+            this.x = x;
+    }
+
+    public void         setX(char letter)
+    {
+        int a = CharIntMap.getIndex(letter);
+        if (a > 0)
+        {
+            this.x = a;
+        }
+    }
+
+    public void         setY(int y)
+    {
+        if (y >= 0 && y < LIMIT)
+            this.y = y;
+    }
+
+    public void         setY(char letter)
+    {
+        int a = CharIntMap.getIndex(letter);
+        if (a > 0)
+        {
+            this.y = a;
+        }
+    }
+
+
+    // Console accessors
+    public String       toChess()
     {
         return String.format("%s%d",CharIntMap.getChar(x), y+1);
     }
 
-    public String toString()
+    public String       toString()
     {
         return String.format("%d,%d",x,y);
     }
+
 
 
 //    public double getSlope(Point other)
